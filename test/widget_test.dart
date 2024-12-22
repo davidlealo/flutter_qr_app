9b01d7b8a7833dad027337452943d73ddaa27786
davidlealo <davidlealo@gmail.com>
2024-12-21 22:13:09 -0300

    cambio en test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,22 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_qr_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Navigation to QR Scanner works', (WidgetTester tester) async {
+    // Construye la app y activa un fotograma.
+    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verifica que estamos en la pantalla principal.
+    expect(find.text('cámara QR'), findsOneWidget);
+    expect(find.text('Abrir cámara'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Simula un tap en el botón "Abrir cámara".
+    await tester.tap(find.text('Abrir cámara'));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verifica que hemos navegado a la pantalla de escaneo de QR.
+    expect(find.text('Escanear QR'), findsOneWidget);
   });
 }
