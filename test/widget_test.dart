e626415d4dfab2eac80af1fdad21b2df68d3de0c
davidlealo <davidlealo@gmail.com>
2024-12-22 22:23:34 -0300

    cambio en test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,19 +4,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_qr_app/main.dart';
 
 void main() {
-  testWidgets('Navigation to QR Scanner works', (WidgetTester tester) async {
+  testWidgets('Navigation and invalid code handling', (WidgetTester tester) async {
     // Construye la app y activa un fotograma.
     await tester.pumpWidget(MyApp());
 
     // Verifica que estamos en la pantalla principal.
     expect(find.text('cámara QR'), findsOneWidget);
-    expect(find.text('Abrir cámara'), findsOneWidget);
+    expect(find.text('Validar código'), findsOneWidget);
 
-    // Simula un tap en el botón "Abrir cámara".
-    await tester.tap(find.text('Abrir cámara'));
+    // Simula un tap en el botón de validar con código incorrecto.
+    await tester.enterText(find.byType(TextField), '654321');
+    await tester.tap(find.text('Validar código'));
+    await tester.pump();
+
+    // Verifica que se muestra un mensaje de error.
+    expect(find.text('El código ingresado es incorrecto.'), findsOneWidget);
+
+    // Ingresa el código correcto.
+    await tester.enterText(find.byType(TextField), '123456');
+    await tester.tap(find.text('Validar código'));
     await tester.pumpAndSettle();
 
-    // Verifica que hemos navegado a la pantalla de escaneo de QR.
-    expect(find.text('Escanear QR'), findsOneWidget);
+    // Verifica que se navega a la pantalla de resultados.
+    expect(find.text('Resultado Escaneado'), findsOneWidget);
+    expect(find.text('123456'), findsOneWidget);
   });
 }
